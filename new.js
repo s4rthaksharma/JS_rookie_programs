30a312135a452ed17e1c6a6d857d5ec5f28c9919
Sarthak <zackwillcrack@gmail.com>
2023-12-17 20:35:13 +0530

    JS Files

---
+++ new.js
@@ -0,0 +1,23 @@
+function fun1()
+{
+    console.log("testing")
+    x=document.getElementById('one')
+        x.style.color='white'
+        x.style.background='black'
+}
+function fun()
+{
+    console.log("hi")
+    x=document.getElementById('para')
+    if(x.style.display!="none")
+    {
+        x.style.display="none"
+        
+    }
+    else
+    {
+        x.style.display="block"
+    }
+      
+
+}
